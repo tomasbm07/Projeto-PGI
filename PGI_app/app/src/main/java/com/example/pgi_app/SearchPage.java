4e9d13dcd51f74dbadc5523846cd34225fa90350
Miguel Pedroso <miguelpedroso@Miguels-MacBook-Pro.local>
2021-12-05 23:30:55 +0000

    searchPage and plantPage updated

--- PGI_app/app/src/main/java/com/example/pgi_app/SearchPage.java
+++ PGI_app/app/src/main/java/com/example/pgi_app/SearchPage.java
@@ -14,30 +14,42 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.Serializable;
 
 public class SearchPage extends AppCompatActivity {
 
-    ListView listview;
-
+    ListView listView;
     ArrayAdapter<Planta> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
-
-        listview = findViewById(R.id.listview);
+        Intent i = new Intent(this, PlantPage.class);
+        listView = findViewById(R.id.listview);
 
         arrayAdapter = new ArrayAdapter<Planta>(this, android.R.layout.simple_list_item_1, Planta.plantList);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                //TextView c = (TextView) v.findViewById(R.id.search_menu);
+                String name = (String) ((TextView) v).getText();
+                Toast.makeText(getApplicationContext(),name + " selected", Toast.LENGTH_SHORT).show();
+                Planta p = Planta.getPlanta(name);
+                System.out.println(p.getCuidados());
+                i.putExtra("sample object", p);
+                startActivity(i);
+
+            }
+        });
+
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
-
         MenuItem menuItem = menu.findItem(R.id.search_menu);
         SearchView searchView = (SearchView) menuItem.getActionView();
         searchView.setQueryHint("Type plant name...");
@@ -57,13 +69,9 @@ public class SearchPage extends AppCompatActivity {
         });
         return super.onCreateOptionsMenu(menu);
     }
-    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-        TextView c = (TextView) v.findViewById(R.id.search_menu);
-        String name = c.getText().toString();
-        Planta p = Planta.getPlanta(name);
-        Intent i = new Intent(this, PlantPage.class);
-        i.putExtra("sample object", (Serializable) p);
-        startActivity(i);
-        Toast.makeText(getApplicationContext(),name + " selected", Toast.LENGTH_SHORT).show();
-    }
+
+
+
+
+
 }
