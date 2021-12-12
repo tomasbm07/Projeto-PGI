7a22bc625bc719301b5099244f1fdca5b23e6b5d
Tomás Mendes <43079732+tomasbm07@users.noreply.github.com>
2021-12-12 11:59:42 +0000

    minor changes

--- Web_Server/Server/projeto_pgi/settings.py
+++ Web_Server/Server/projeto_pgi/settings.py
@@ -116,8 +116,8 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = "static"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "web/static")]
 
