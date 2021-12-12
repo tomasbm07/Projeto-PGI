6d17ed8182910f25d9b1af38572a6a994dded286
Alexandre Andrade <alex.d.a_14@outlook.pt>
2021-12-12 02:44:17 +0000

    feat: gui para sqlite, guardar dados na bd, form a funcionar

--- Web_Server/Server/web/models.py
+++ Web_Server/Server/web/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact_form(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+    message = models.CharField(max_length=16084)
--- Web_Server/Server/web/views.py
+++ Web_Server/Server/web/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from .models import Contact_form
 
 
 def homepage(request):
-    return render(request, "web/index.html")
+
+    if request.method == "GET":
+        return render(request, "web/index.html")
+    elif request.method == "POST":
+        if request.POST.get("form"):
+            print(request.POST["name"])
+            print(request.POST["email"])
+            print(request.POST["msg"])
+
+            info = Contact_form(name=request.POST["name"], email=request.POST["email"], message=request.POST["msg"])
+            info.save()
+        
+        return render(request, "web/index.html")
 
